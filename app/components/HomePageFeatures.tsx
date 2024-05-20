c111994357acb7feef54af684427138ce45d6627
Nicholas Ameyaw Abuaku <abuaku1400@gmail.com>
2024-05-20 18:57:37 +0100

    Created section for home page called features

---
+++ app/components/HomePageFeatures.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+const HomePageFeatures = () => {
+  return (
+    <Box
+      sx={{
+        backgroundColor: "#FB610A",
+        height: "25vh",
+        width: "99vw",
+        textAlign: "center",
+      }}
+    >
+      <Typography variant="h3">Features</Typography>
+    </Box>
+  );
+};
+
+export default HomePageFeatures;
